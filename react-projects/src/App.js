0608eb8415fa339f9dc550de480f9308e5134f84
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-11-18 16:09:12 +0000

    side bar added

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 import BusinessCardHome from "../src/pages/businessCard/BusinessCardHome";
 import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
 
 function App() {
     return (
         <>
             <Navbar />
+            <SideBar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/businessCard" element={<BusinessCardHome />} />
--- react-projects/src/components/SideBar.js
+++ react-projects/src/components/SideBar.js
@@ -1,8 +1,45 @@
 import React from "react";
 import styles from "./sidebar.module.css";
 
+const projectsList = [
+    {
+        id: 1,
+        title: "Business Card",
+        url: "pathway1",
+    },
+    {
+        id: 2,
+        title: "AirBnB Page",
+        url: "pathway2",
+    },
+    {
+        id: 3,
+        title: "Notes App",
+        url: "pathway3",
+    },
+];
+
 const SideBar = () => {
-    return <div className={styles.container}>SideBar</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>Projects.list</div>
+            <div className={styles.project}>
+                {projectsList.map((project, i) => {
+                    return (
+                        <div key={i} className={styles.project_item}>
+                            <a
+                                href={project.url}
+                                className={styles.project_title}
+                            >
+                                {project.title}
+                            </a>
+                        </div>
+                    );
+                })}
+            </div>
+            <p className={styles.coming_soon}>...more on the way!</p>
+        </div>
+    );
 };
 
 export default SideBar;
