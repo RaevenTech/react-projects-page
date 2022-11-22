317c08e7334e72aa18d4c254a3f1f66b1f98aab6
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-11-22 11:12:04 +0000

    landing page layout complete

--- react-projects/src/pages/Home.js
+++ react-projects/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
 import styles from "./home.module.css";
+import reactLogo from "../assets/logo512.png";
 //import BusinessCardHome from "./businessCard/BusinessCardHome"; //<BusinessCardHome />
 
 const Home = () => {
     return (
         <div className={styles.container}>
-            <h1>Landing/Home page</h1>
+            <h1 className={styles.title1}>
+                React.<span className={styles.projectsTxt}>projects</span>
+            </h1>
+            <img
+                className={styles.react_logo}
+                src={reactLogo}
+                alt="react logo"
+            />
+            <h1 className={styles.title2}>
+                React-<span className={styles.routerTxt}>Router</span>-
+                <span className={styles.domTxt}>DOM</span>
+            </h1>
         </div>
     );
 };
