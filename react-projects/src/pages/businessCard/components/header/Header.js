42bfe1c5de93037528329d3d6bfe19bfba5e2e80
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-11-23 09:40:43 +0000

    business card complete

--- react-projects/src/pages/businessCard/components/header/Header.js
+++ react-projects/src/pages/businessCard/components/header/Header.js
@@ -4,28 +4,32 @@ import profileImg from "./assets/linkedin-profile-pic2.jpg";
 import { BsLinkedin } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 
-//https://picsum.photos/200/300
-
 const Header = () => {
     return (
         <div className={styles.container}>
             <img className={styles.img} src={profileImg} alt="profile" />
             <h1 className={styles.name}>Raeven Goncalves</h1>
             <h3 className={styles.job_title}>Front-End Developer</h3>
-            <p className={styles.website}>raeven.website</p>
+            <a href="https://raeven-portfolio.vercel.app">
+                <p className={styles.website}>raeven.website</p>
+            </a>
             <div className={styles.btns_el}>
-                <button className={styles.btn}>
-                    <div className={styles.btn_icon}>
-                        <MdEmail />
-                    </div>
-                    <div className={styles.btn_txt}>Email</div>
-                </button>
-                <button className={styles.btn1}>
-                    <div className={styles.btn_icon1}>
-                        <BsLinkedin />
-                    </div>
-                    <div className={styles.btn_txt}>Linkedin</div>
-                </button>
+                <a href="mailto:raevengoncalves@gmail.com">
+                    <button className={styles.btn}>
+                        <div className={styles.btn_icon}>
+                            <MdEmail />
+                        </div>
+                        <div className={styles.btn_txt}>Email</div>
+                    </button>
+                </a>
+                <a href="www.linkedin.com">
+                    <button className={styles.btn1}>
+                        <div className={styles.btn_icon1}>
+                            <BsLinkedin />
+                        </div>
+                        <div className={styles.btn_txt}>Linkedin</div>
+                    </button>
+                </a>
             </div>
         </div>
     );
