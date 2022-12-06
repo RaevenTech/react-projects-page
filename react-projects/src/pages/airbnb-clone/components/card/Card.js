f3892a88c7e36bcfb1862dbb33c4271fe6696cad
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-12-06 10:10:53 +0000

    card section

--- react-projects/src/pages/airbnb-clone/components/card/Card.js
+++ react-projects/src/pages/airbnb-clone/components/card/Card.js
@@ -11,30 +11,36 @@ const Card = (props) => {
     }
 
     return (
-        <section className={styles.cards}>
-            <div className={styles.image}>
-                {badgeText && <div className={styles.badge}>{badgeText}</div>}
-                <img
-                    className={styles.img}
-                    src={props.card.coverImg}
-                    alt={props.card.title}
-                    width={250}
-                    height={355}
-                />
-            </div>
-            <div className={styles.rating_info}>
-                <img src={star} alt="Star" className={styles.star} />
-                <p className={styles.rating}>{props.card.stats.rating}</p>{" "}
-                <p className={styles.gray_info}>
-                    ({props.card.stats.reviewCount})-{props.card.location}
+        <div className={styles.container}>
+            <section className={styles.cards}>
+                <div className={styles.image}>
+                    {badgeText && (
+                        <div className={styles.badge}>{badgeText}</div>
+                    )}
+                    <img
+                        className={styles.img}
+                        src={props.card.coverImg}
+                        alt={props.card.title}
+                        width={250}
+                        height={355}
+                    />
+                </div>
+                <div className={styles.rating_info}>
+                    <img src={star} alt="Star" className={styles.star} />
+                    <p className={styles.rating}>
+                        {props.card.stats.rating}
+                    </p>{" "}
+                    <p className={styles.gray_info}>
+                        ({props.card.stats.reviewCount})-{props.card.location}
+                    </p>
+                </div>
+                <p className={styles.title}>{props.card.title}</p>
+                <p className={styles.price_person}>
+                    <span className={styles.price}>From ${props.price} </span> /
+                    person
                 </p>
-            </div>
-            <p className={styles.title}>{props.card.title}</p>
-            <p className={styles.price_person}>
-                <span className={styles.price}>From ${props.price} </span> /
-                person
-            </p>
-        </section>
+            </section>
+        </div>
     );
 };
 
